a596c33c96d740e6bf92563282936e1e2cfa193b
korjsh <korjsh91@gmail.com>
2024-11-29 22:03:00 +0900

    update: 로그 추가

--- app/database.py
+++ app/database.py
@@ -3,14 +3,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 import sys
+import logging
+logger = logging.getLogger("uvicorn")
 
 if getattr(sys, "frozen", False):  # 패키징된 상태인지 확인
     BASE_DIR = os.path.dirname(sys.executable)  # 실행 파일의 디렉토리
 else:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'test.db')}"
-print("Base Directory:", BASE_DIR)
-print("Database Path:", SQLALCHEMY_DATABASE_URL)
+logger.info(f"Base Directory:{BASE_DIR}")
+logger.info(f"Database Path:{SQLALCHEMY_DATABASE_URL}")
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import logging
+from typing import AsyncIterator
+
 from fastapi import FastAPI
 from app.routers import auth, movie, user, bookmark, rating  # 각 라우터 가져오기
 from app.database import engine
 from app.models import movie as movie_model, user as user_model, bookmark as bookmark_model, rating as rating_model   # 모델 가져오기
+logger = logging.getLogger("uvicorn.info")
 
-app = FastAPI()
+async def lifespan_handler(app: FastAPI) -> AsyncIterator[None]:
+    # 애플리케이션 시작 시 실행
+    logger.info("Starting MovieMark API server...")
+    logger.info("Swagger UI available at: http://0.0.0.0:8000/docs")
+    logger.info("ReDoc available at: http://0.0.0.0:8000/redoc")
+
+    yield  # 앱이 실행 중인 동안 여기서 대기
+
+    # 애플리케이션 종료 시 실행
+    logger.info("Shutting down MovieMark API server...")
+app = FastAPI(lifespan=lifespan_handler)
 
 # 데이터베이스 초기화
 movie_model.Base.metadata.create_all(bind=engine)
@@ -19,7 +33,7 @@ app.include_router(user.router, prefix="/users", tags=["Users"])
 app.include_router(bookmark.router, prefix="/bookmarks", tags=["Bookmarks"])
 app.include_router(rating.router, prefix="/ratings", tags=["Ratings"])
 
+
 if __name__ == "__main__":
-    print("Starting MovieMark API server...")
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
