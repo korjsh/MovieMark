56754ae6f7c65a8ea2a27e0ae4bd3fe63de7213c
jin yi <jin12cc@gmail.com>
2024-12-05 18:10:40 +0900

    update: 윈도우 실행 패키지 환경변수 추가 스크립트

--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Building executable for Windows...
 
-pyinstaller --onefile --hidden-import=passlib.handlers.bcrypt --name my_fastapi_server main.py
+pyinstaller --onefile --hidden-import=passlib.handlers.bcrypt --name my_fastapi_server --add-data ".env;." main.py
 
 if %ERRORLEVEL% NEQ 0 (
     echo Error occurred during the build process.
